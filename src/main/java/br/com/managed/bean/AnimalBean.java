affe18431fbc308ce658db552455803f0114cb92
Fernando Menolli <femenolli@hotmail.com>
2013-11-11 21:23:43 -0200

    Agora vai

---
+++ src/main/java/br/com/managed/bean/AnimalBean.java
@@ -0,0 +1,38 @@
+package br.com.managed.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import br.com.sisbov.dao.impl.AnimalDao;
+import br.com.sisbov.persistence.Animal;
+
+
+
+
+public class AnimalBean {
+	
+	private Animal animal = new Animal();
+
+	public Animal getAnimal() {
+		return animal;
+	}
+	
+	public void inserir() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		try {
+			AnimalDao dao = new AnimalDao();
+			dao.save(animal);
+			
+			context.addMessage(null,
+					new FacesMessage("Inserção finalizada."));
+		} catch (Exception e) {
+			context.addMessage(null,
+					new FacesMessage("Erro ao inserir: " + e.getMessage()));
+		}
+	} 
+
+	public Sexo[] getUfValues() {
+		return Uf.values();
+	}
+
+}

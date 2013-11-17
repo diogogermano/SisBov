59e0d186624cc1d7c14863aba2f855060fd51073
=Germano <=germanuu@gmail.com>
2013-11-17 19:04:41 -0200

    Adicionado botao voltar na tela animal, LoteTest nao funcionando

---
+++ src/test/java/br/com/sisbov/test/LoteTest.java
@@ -0,0 +1,31 @@
+package br.com.sisbov.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import br.com.sisbov.dao.impl.LoteDao;
+import br.com.sisbov.persistence.Animal;
+import br.com.sisbov.persistence.Lote;
+import br.com.sisbov.persistence.Piquete;
+
+public class LoteTest {
+
+	@Test
+	public void test() {
+		Lote lote = new Lote();
+		Piquete piquete = new Piquete();
+		Animal anim = new Animal();
+		
+		lote.setPiquete(piquete);
+		lote.setAnimal(anim);
+		
+		LoteDao dao = new LoteDao();
+		
+		dao.save(lote);
+	}
+
+}
